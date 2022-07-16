2f63a583d19223b2b24dbbc0a425c0e80323f353
RealSadLemon <esmodearburk@gmail.com>
2022-07-16 18:31:38 -0500

    Set up GET functionality for /api/actions(/:id)

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,31 @@
 // Write your "actions" router here!
+const express = require('express');
+const router = express.Router();
+const actionsModel = require('./actions-model');
+
+router.get('/', (req, res) => {
+    actionsModel.get()
+        .then(actions => {
+            if(actions == []){
+                res.status(404).json({ message: "action not found." });
+                return;
+            } else {
+                res.status(200).json(actions);
+                return;
+            }
+        })
+})
+
+router.get('/:id', (req, res) => {
+    actionsModel.get(req.params.id)
+        .then(action => {
+            if(action === null){
+                res.status(404).json({ message: "action not found." });
+                return;
+            } else {
+                res.status(200).json(action)
+            }
+        })
+})
+
+module.exports = router;
